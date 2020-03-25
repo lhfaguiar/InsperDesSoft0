b85c119336167c9288c5eade7cac3d7f3549db64
lhfaguiar <lais.harumi.f@gmail.com>
2020-03-24 22:30:59 -0300

    estrutura de regras

--- CrapsInsper.py
+++ CrapsInsper.py
@@ -1,1 +1,47 @@
-#
+###############################################
+## Exercicio Programa 1 - Design de Software ##
+##                                           ##
+## author: @lhfaguiar                        ##
+## author: @
+###############################################
+
+
+
+fase = 0
+fases = {0: 'ComeOut', 1: 'Point'}
+if (fichas == 0):
+    print('Sair')
+    
+else:
+    print('JOGO')
+    
+    
+    #come out
+    
+    
+    #point
+    
+def pass_line_bet(soma):
+    
+    if (soma==7) or (soma==11):
+        print('Venceu')
+        
+    elif (soma==2) or (soma==3) or (soma==12):
+        print('Perdeu')
+        
+    else:
+        print('Point')
+        
+def field():
+    
+    print('field')
+
+
+def any_craps():
+    
+    print('any_craps')
+    
+
+def twelve():
+    
+    print('twelve')
